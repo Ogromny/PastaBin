9f42b74a36ba35ff3e8ad6756d6b7fdb09897a38
Ogromny <ogromny@openmailbox.org>
2016-04-14 18:50:55 +0200

    Ajout de encrypt.d pour plus de propreter

---
+++ source/encrypt.d
@@ -0,0 +1,14 @@
+module Encrypt;
+
+/*
+    Je vais utiliser AES grace à OpenSSL
+    Donc wait l'amis ça vient
+ */
+
+string encrypt_string(string content, string password) @trusted {
+    return content;
+}
+
+string decrypt_string(string content, string password) @trusted {
+    return content;
+}
